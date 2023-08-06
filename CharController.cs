182b1b385007c8031f5e3b8894b333388bf80467
youmijo <54327358+youmijo@users.noreply.github.com>
2023-08-06 21:42:15 +0900

    Update CharController.cs

--- CharController.cs
+++ CharController.cs
@@ -6,6 +6,7 @@ public class CharController : MonoBehaviour
 {
     [SerializeField]
     private Transform characterBody;
+    
     [SerializeField]
     private Transform cameraArm;
     public GameManager manager; 
@@ -29,10 +30,10 @@ public class CharController : MonoBehaviour
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         bool isMove = manager.isAction || manager.isEsc || manager.isStart ? false : moveInput.magnitude != 0;
-        //animator.SetBool("isMove", isMove);
 
         if (isMove)
-        { Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
+        { 
+            Vector3 lookForward = new Vector3(cameraArm.forward.x, 0f, cameraArm.forward.z).normalized;
             Vector3 lookRight = new Vector3(cameraArm.right.x, 0f, cameraArm.right.z).normalized;
             Vector3 moveDir = lookForward * moveInput.y + lookRight * moveInput.x;
             characterBody.forward = lookForward; transform.position += moveDir * Time.deltaTime * 5f;
@@ -59,5 +60,4 @@ public class CharController : MonoBehaviour
 
         cameraArm.rotation = Quaternion.Euler(x, camAngle.y + mouseDelta.x, camAngle.z);
     }
-
 }
